e1bcdeeca24123c7700bcb36ad8e0bd5334fbfbe
Shobana Vijayakumar <shobana@Shobanas-MBP.attlocal.net>
2023-09-30 18:37:09 -0500

    final commit

--- GlobalStyles.tsx
+++ GlobalStyles.tsx
@@ -64,3 +64,26 @@ export const StyledFlex = styled.div`
     align-items: center;
   }
 `;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+export const SuccessMessage = styled.h2`
+  font-size: 2rem;
+  color: #28a745;
+  animation: fadeIn 1s ease;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
--- components/Submit.tsx
+++ components/Submit.tsx
@@ -1,27 +1,12 @@
 import React from 'react'
-import styled from 'styled-components';
-
-const StyledBanner = styled.div`
-  background-color: #4caf50;
-  color: white;
-  padding: 16px;
-  text-align: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-}`;
-
-const Styledh1 = styled.h1`
-   text-align: center;
-   margin-top: 50px;
-`;
+import { Container } from '@/GlobalStyles';
+import { SuccessMessage } from '@/GlobalStyles';
 
 const Submit = () => {
   return (
-   <StyledBanner>
-      <Styledh1>Form Submitted Sucessfully!</Styledh1>
-    </StyledBanner>
+    <Container>
+    <SuccessMessage>Form Submitted Successfully!</SuccessMessage>
+  </Container>
   )
 }
 
